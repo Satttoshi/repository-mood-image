f289a32221de47dce37b9a4a2831e369a53fc926
Joshua Stolle <joshuastolle445@gmail.com>
2023-11-08 13:28:36 +0100

    feat(INTPRO-514): some prettier shenanigans

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
